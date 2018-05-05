675e8dabc71e13e78ccd53f83ee353c1e0a33a36
佐橘一旗 <1tsuki@RLS01002552M01.local>
2018-05-05 22:27:58 +0900

    added contents for 04_css

--- tools/evaluate04.js
+++ tools/evaluate04.js
@@ -5,18 +5,18 @@ const tools = require("./tools");
 process.setMaxListeners(0);
 
 // create export dir
-const workDir = tools.createWorkDirSync("04_contents", process.argv[2]);
+const workDir = tools.createWorkDirSync("04_css", process.argv[2]);
 
 // evaluate files for each users
 const config = require("./config.json");
+console.log("今回の課題は教員/SAによる最終確認が必要です")
 config.users.forEach(function(userId, index, array) {
-  console.log(userId);
-  // evaluate03_7(userId);
+  evaluate04(userId);
 });
 
-function evaluate03_7(userId) {
-  const taskId = "ex03-7"
-  const targetUrl = "http://web.sfc.keio.ac.jp/~" + userId + "/ex03/report.html";
+function evaluate04(userId) {
+  const taskId = "ex04"
+  const targetUrl = "http://web.sfc.keio.ac.jp/~" + userId + "/ex04.html";
   const htmlPath = workDir + userId + "_" + taskId + ".html";
   const resultPath = workDir + userId + "_" + taskId + ".log";
 
@@ -35,17 +35,33 @@ function evaluate03_7(userId) {
       errors.push("Invalid status code: " + response.status())
     } else {
       tools.writeFile(htmlPath, body);
-      if (await page.$("p") == null) errors.push("<p> not found");
-      if (await page.$("br") == null) errors.push("<br> found");
-      if (await page.$("a") == null) errors.push("<a>not found");
-      if (body.length < 400) errors.push("report is too short");
+
+      // check existence of img tag and src
+      const imgs = await page.$$eval('img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+      if (imgs.length < 1) {
+        errors.push("<img> not found");
+      } else {
+        imgs.forEach(function(src) {
+          if (src.startsWith("http://") || src.startsWith("https://")) {
+            if (!src.startsWith("http://web.sfc.keio.ac.jp") && !src.startsWith("https://web.sfc.keio.ac.jp")) {
+              errors.push("<img> is reffering to other domains contents");
+            }
+          }
+        });
+      }
+
+      if (await page.$("link") == null) errors.push("<link> found");
+      if (await page.$("table") == null) errors.push("<table> found");
+      if (await page.$("tr") == null) errors.push("<td> found");
+      if (await page.$("th") == null) errors.push("<th> found");
+      if (await page.$("td") == null) errors.push("<td> found");
     }
 
     browser.close();
 
     // output result
     if (errors.length > 0) {
-      tools.writeFile(resultPath + ".fail", errors);
+      tools.writeFile(resultPath, errors);
     } else {
       console.log(userId + " passed " + taskId)
     }
